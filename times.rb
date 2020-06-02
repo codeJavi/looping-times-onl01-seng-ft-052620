823db4595d52d1cc43a43dbf3a39e7a259020964
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 14:27:36 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,5 +1,5 @@
 describe "#using_times" do
-	"puts out the phrase 'Wingardium Leviosa' 7 times by using the 'times' construct" do
+it "puts out the phrase 'Wingardium Leviosa' 7 times by using the 'times' construct" do
 
 expect{using_times}.to output (looping_string).to_stdout
 end 
