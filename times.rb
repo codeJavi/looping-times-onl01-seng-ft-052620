d70fc8ec197ea3b48862180269ffb1eb823ad59c
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 14:39:48 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,9 +1,9 @@
 7.times do
  it "puts out the phrase 'Wingardium Leviosa' 7 times by using the 'times' construct" do
 
-loop_string = "Wingardium Leviosa"
+
 expect{ using_times }.to output(looping_string).to_stdout
-end 
+
 
 
 
