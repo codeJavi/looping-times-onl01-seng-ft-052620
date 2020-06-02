b9b72ac6a38c2758f7b3bce2fbd62c8b9777aed7
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 14:44:56 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,9 +1,9 @@
-def #using_times 
+def using_times 
   7.times do
     puts "Wingardium Leviosa"
   end 
 
-using_times 
+
 Looping_string = "Wingardium Leviosa"
 expect{ using_times }.to output(looping_string).to_stdout
 end 
